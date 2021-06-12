51176e8dcb3612992522ec2da214402a085ed468
psrajoria <psrajoria@outlook.com>
2021-06-13 00:25:28 +0530

    Parameterised input to URL

--- src/hello/views.py
+++ src/hello/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse("Hello World")
+
+def greet(request,name):
+    return HttpResponse(f"Hello, {name.capitalize()}")
