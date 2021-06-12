cc854b28686719136f68efe3b17aaf3aa8f822af
psrajoria <psrajoria@outlook.com>
2021-06-13 00:43:56 +0530

    Rendering template with variable

--- src/hello/views.py
+++ src/hello/views.py
@@ -5,5 +5,19 @@ from django.http import HttpResponse
 def index(request):
     return HttpResponse("Hello World")
 
-def greet(request,name):
-    return HttpResponse(f"Hello, {name.capitalize()}")
+# def greet(request,name):
+#     return httpresponse(f"hello, {name.capitalize()}")
+
+
+# def greet(request, name, template_name="hello/greet.html"):
+#     #context = 
+#     return render(request,  template_name, {
+#         "name": name.capitalize()
+#     })
+
+
+def greet(request, name):
+    #context =
+    return render(request,  "hello/greet.html", {
+        "name": name.capitalize()
+    })
