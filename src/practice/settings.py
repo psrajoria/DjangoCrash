f3002f387ad1fb5cf073f757c86f0a9373229bc2
psrajoria <psrajoria@outlook.com>
2021-06-13 01:19:29 +0530

    Rendering conditions in templates

--- src/practice/settings.py
+++ src/practice/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     
     #my created apps
     'hello',
+    'isitchristmas',
 ]
 
 MIDDLEWARE = [
--- src/practice/urls.py
+++ src/practice/urls.py
@@ -19,5 +19,6 @@ from django.urls.conf import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("hello", include("hello.urls"))
+    path("hello", include("hello.urls")),
+    path("isitchristmas", include("isitchristmas.urls"))
 ]
