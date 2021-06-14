631e26eab6eb3f43b24a420132040120bc90ecc3
psrajoria <psrajoria@outlook.com>
2021-06-14 23:44:27 +0530

    Add tasks (Create)

--- src/tasks/views.py
+++ src/tasks/views.py
@@ -1,14 +1,35 @@
-from django.shortcuts import render
+from django.forms.fields import CharField
+from django.shortcuts import redirect, render
+from django import forms
 
 
-tasks = ["foo","boo","doo"]
+class NewTaskForm(forms.Form):
+    task = forms.CharField(max_length=20, label="New Task",
+                           widget=forms.TextInput(attrs={'placeholder': 'Task'}))
+
+
+tasks = ["foo", "boo", "doo"]
 
 # Create your views here.
 
+
 def index(request):
-    return render(request,"tasks/index.html",{
-        'tasks':tasks
+    return render(request, "tasks/index.html", {
+        'tasks': tasks
     })
 
+
 def add_task(request):
-    return render(request,"tasks/add.html")
+    if request.method == "POST":
+        form = NewTaskForm(request.POST)
+        if form.is_valid():
+            task = form.cleaned_data['task']
+            tasks.append(task)
+        else:
+            render(request, "tasks/add.html", {
+                'form': form
+            })
+
+    return render(request, "tasks/add.html", {
+        'form': NewTaskForm()
+    })
